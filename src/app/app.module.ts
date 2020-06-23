00fe83e7eb90aeff61e50765ac6e9699838f5cd2
brianlynch <brian.lynch@liberty-it.co.uk>
2020-06-23 19:14:13 +0100

    tets

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      { path: '', component: LandingPageComponent },
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
