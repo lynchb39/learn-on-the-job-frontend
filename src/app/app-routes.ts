5b06e2e7711e2f83b36211462f81d19179c0753b
brianlynch <brian.lynch@liberty-it.co.uk>
2020-07-20 18:48:54 +0100

    updates

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -3,6 +3,7 @@ import {LandingPageComponent} from './landing-page/landing-page.component';
 
 
 export const routes: Routes = [
-  { path: 'landing-page', component: LandingPageComponent },
-  { path: 'login-page', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent },
+  { path: '', component: LandingPageComponent },
 ];
